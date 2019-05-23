2baae12ceff05fb2df55bb85f9c9b6bb07745e92
B0RlS <Dosya0318@gmail.com>
2019-05-23 14:15:22 +0300

    Add User model with validation

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase}
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 30}
+  validates :surname, presence: true, length: { minimum: 5, maximum: 50}
+  validates :email, presence: true, length: { minimum: 10, maximum: 200},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
---
+++ db/migrate/20190523084941_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.string :email
+      t.integer :profile_id
+      t.integer :role_id
+
+      t.timestamps
+    end
+  end
+end
